f3fe5a0f372f19a2775fbd1ac262aa72ebdfb763
nyarian <nyarian83@gmail.com>
2022-01-12 20:27:49 +0200

    Migrate from deprecated LifecycleListener APIs

--- android/src/main/java/tv/nexx/flutter/android/platform_view/NexxPlayLifecycleAdapter.java
+++ android/src/main/java/tv/nexx/flutter/android/platform_view/NexxPlayLifecycleAdapter.java
@@ -1,12 +1,26 @@
 package tv.nexx.flutter.android.platform_view;
 
+import androidx.annotation.NonNull;
 import androidx.lifecycle.Lifecycle;
-import androidx.lifecycle.LifecycleObserver;
-import androidx.lifecycle.OnLifecycleEvent;
+import androidx.lifecycle.LifecycleEventObserver;
+import androidx.lifecycle.LifecycleOwner;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import tv.nexx.android.play.INexxPLAY;
+import tv.nexx.flutter.android.estd.functional.Consumer;
 
-class NexxPlayLifecycleAdapter implements LifecycleObserver {
+class NexxPlayLifecycleAdapter implements LifecycleEventObserver {
+
+    private static final Map<Lifecycle.Event, Consumer<INexxPLAY>> MAPPING = new HashMap<>();
+
+    static {
+        MAPPING.put(Lifecycle.Event.ON_RESUME, INexxPLAY::onActivityResume);
+        MAPPING.put(Lifecycle.Event.ON_PAUSE, INexxPLAY::onActivityPause);
+        MAPPING.put(Lifecycle.Event.ON_DESTROY, INexxPLAY::onActivityDestroyed);
+    }
+
     private final INexxPLAY player;
 
     private NexxPlayLifecycleAdapter(INexxPLAY player) {
@@ -17,18 +31,13 @@ class NexxPlayLifecycleAdapter implements LifecycleObserver {
         return new NexxPlayLifecycleAdapter(player);
     }
 
-    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
-    public void onResume() {
-        player.onActivityResume();
+    @Override
+    public void onStateChanged(@NonNull LifecycleOwner source,
+                               @NonNull Lifecycle.Event event) {
+        final Consumer<INexxPLAY> consumer = MAPPING.get(event);
+        if (consumer != null) {
+            consumer.accept(player);
+        }
     }
 
-    @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
-    public void onPause() {
-        player.onActivityPause();
-    }
-
-    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
-    public void onDestroy() {
-        player.onActivityDestroyed();
-    }
 }
