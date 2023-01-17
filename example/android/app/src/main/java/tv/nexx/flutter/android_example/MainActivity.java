8c6baf348e5c0f3ad7d3301d1d65c66300ac1222
nyarian <orkay255@gmail.com>
2023-01-17 22:37:26 +0100

    Update README, cleanup the documentation, cleanup the Chromecast config

--- example/android/app/src/main/java/tv/nexx/flutter/android_example/MainActivity.java
+++ example/android/app/src/main/java/tv/nexx/flutter/android_example/MainActivity.java
@@ -3,8 +3,13 @@ package tv.nexx.flutter.android_example;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.media.session.MediaSessionCompat;
+//import android.util.Log;
 
 import androidx.annotation.Nullable;
+
+//import com.google.android.gms.cast.framework.CastContext;
+
+//import java.util.concurrent.Executors;
 
 import io.flutter.embedding.android.FlutterActivity;
 import tv.nexx.android.admanager.NexxPlayAdManager;
@@ -14,12 +19,16 @@ import tv.nexx.flutter.android.android.event.OnUserLeaveHintEvent;
 
 public class MainActivity extends FlutterActivity {
 
+    @SuppressWarnings("CommentedOutCode")
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         final MediaSessionCompat mediaSession = new MediaSessionCompat(getApplicationContext(), getPackageName());
         NexxPlayPlugin.addEnvironmentConfigurationEntry(NexxPlayPlugin.KEY_MEDIA_SESSION, mediaSession);
         NexxPlayPlugin.addEnvironmentConfigurationEntry(NexxPlayPlugin.KEY_AD_MANAGER, NexxPlayAdManager::new);
+//        CastContext.getSharedInstance(this, Executors.newSingleThreadExecutor())
+//                .addOnSuccessListener(castContext -> NexxPlayPlugin.addEnvironmentConfigurationEntry(NexxPlayPlugin.KEY_CAST_CONTEXT, castContext))
+//                .addOnFailureListener(exception -> Log.e("nexxPLAY", "Could not resolve CastContext", exception));
     }
 
     @Override
